bbfaeebc2e01088ec9cf362cb561e055de490d58
alphagoal <ac.ting0214@gmail.com>
2021-08-15 23:36:31 +0800

    more comment

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 import copy
 from solver import print_board, sqaure_list,column_list, confirm_change, ops,sol_bo,countdown
 
-
+# define problem
 prob=[[0,0,6,0,9,0,2,0,0],[0,0,0,7,0,2,0,0,0],[0,9,0,5,0,8,0,7,0],[9,0,0,0,3,0,0,0,6],[7,5,0,0,0,0,0,1,9],[1,0,0,0,4,0,0,0,5],[0,1,0,3,0,9,0,8,0],[0,0,0,2,0,1,0,0,0],[0,0,9,0,8,0,1,0,0]]
 org_bo=copy.deepcopy(prob)  #<--- This is deep copy (will not update any changes)
 #org_bo=list(prob) or org_bo= prob.copy() <--- This is shallow copy (will reflect value change)
